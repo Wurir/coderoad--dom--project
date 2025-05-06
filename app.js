a20c1544b8395f3d3de3fe5f12ebfdcd7ce4130a
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-06 14:16:04 +0200

    add createCircle fn

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const circle = document.createElement('div')
+function createCircle(selectorContainer){
+    const container = document.querySelector(selectorContainer)
+    if(!container) return 
 
-circle.style.width = '100px'
-circle.style.height = '100px'
-circle.style.backgroundColor = 'blue'
-circle.style.borderRadius = '50%'
+    const circle = document.createElement('div')
+    
+    circle.style.width = '100px'
+    circle.style.height = '100px'
+    circle.style.backgroundColor = 'blue'
+    circle.style.borderRadius = '50%'
+    
+    container.appendChild(circle)
+}
 
-document.body.appendChild(circle)
+createCircle('body')
